ed8cb8fcb323c4e04dd230b31c075cd87f6e5b71
Slumpjin <tap4302@gmail.com>
2024-04-11 17:32:20 -0500

    Create form to push Book object for MyLibrary

--- js/script.js
+++ js/script.js
@@ -10,6 +10,21 @@ function Book(title, author, numberOfPages, haveRead) {
     }
 }
 
-function addBookToLibrary() {
-  // do stuff here
+myLibrary.push(new Book('Harry Potter', 'J.K. Rowling', 500, false));
+
+const bookForm = document.querySelector('.book-form');
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+    // console.log(formProps);
+    addBookToLibrary(formProps);
+});
+
+function addBookToLibrary(bookInfo) {
+    const newBook = new Book(bookInfo.title, bookInfo.author, 
+        Number(bookInfo.numberOfPages), Boolean(bookInfo.haveRead));
+    myLibrary.push(newBook);
+    // console.log(myLibrary)
 }
+
