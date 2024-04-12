0be67b7fee5d402fa9fc58e4825d83890f47c6a5
Slumpjin <tap4302@gmail.com>
2024-04-11 21:49:54 -0500

    Add table displaying books

--- js/script.js
+++ js/script.js
@@ -5,26 +5,36 @@ function Book(title, author, numberOfPages, haveRead) {
     this.author = author;
     this.numberOfPages = numberOfPages;
     this.haveRead = haveRead;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${numberOfPages} pages, ${this.haveRead ? 'read' : 'not read yet'}`;
-    }
 }
 
 myLibrary.push(new Book('Harry Potter', 'J.K. Rowling', 500, false));
 
 const bookForm = document.querySelector('.book-form');
+const tableBody = document.querySelector('.table-body');
+myLibrary.forEach(book => {
+    addBookToTable(book);
+});
+
 bookForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const formProps = Object.fromEntries(formData);
-    // console.log(formProps);
     addBookToLibrary(formProps);
 });
 
 function addBookToLibrary(bookInfo) {
     const newBook = new Book(bookInfo.title, bookInfo.author, 
-        Number(bookInfo.numberOfPages), Boolean(bookInfo.haveRead));
+    Number(bookInfo.numberOfPages), Boolean(bookInfo.haveRead));
+    addBookToTable(newBook);
     myLibrary.push(newBook);
-    // console.log(myLibrary)
 }
-
+    
+function addBookToTable(book) {
+    const tableRow = document.createElement('tr');
+    Object.values(book).forEach(value => {
+        const tableData = document.createElement('td');
+        tableData.innerText = value;
+        tableRow.appendChild(tableData);
+    });
+    tableBody.appendChild(tableRow);
+}
