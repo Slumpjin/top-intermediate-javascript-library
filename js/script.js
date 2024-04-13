d96a9481f6360add2029417ba24cf0caea68aac6
Slumpjin <tap4302@gmail.com>
2024-04-12 19:37:23 -0500

    Create Add book and have read buttons

--- js/script.js
+++ js/script.js
@@ -9,8 +9,12 @@ function Book(title, author, numberOfPages, haveRead) {
 
 myLibrary.push(new Book('Harry Potter', 'J.K. Rowling', 500, false));
 
+const dialog = document.querySelector('dialog');
 const bookForm = document.querySelector('.book-form');
 const tableBody = document.querySelector('.table-body');
+const addBookButton = document.querySelector('.add-book-button');
+const cancelButton = document.querySelector('.cancel');
+
 myLibrary.forEach(book => {
     addBookToTable(book);
 });
@@ -20,7 +24,19 @@ bookForm.addEventListener('submit', (e) => {
     const formData = new FormData(e.target);
     const formProps = Object.fromEntries(formData);
     addBookToLibrary(formProps);
+    bookForm.reset();
+    dialog.close();
 });
+
+addBookButton.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+cancelButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    bookForm.reset();
+    dialog.close();
+})
 
 function addBookToLibrary(bookInfo) {
     const newBook = new Book(bookInfo.title, bookInfo.author, 
@@ -31,10 +47,28 @@ function addBookToLibrary(bookInfo) {
     
 function addBookToTable(book) {
     const tableRow = document.createElement('tr');
-    Object.values(book).forEach(value => {
+    for (key in book) {
         const tableData = document.createElement('td');
-        tableData.innerText = value;
+        if (key === 'haveRead') {
+            tableData.addEventListener('click', (e) => {
+                tableData.innerText = book[key] = !book[key];
+                const bookIndex = myLibrary.indexOf(book);
+                book[bookIndex] = book;
+            })
+        }
+        tableData.innerText = book[key];
         tableRow.appendChild(tableData);
-    });
+    }
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.addEventListener('click', (e) => {
+        e.target.parentNode.remove();
+        const deletedBookIndex = myLibrary.indexOf(book);
+        myLibrary.splice(deletedBookIndex, 1);
+        console.log(myLibrary);
+    })
+
+    tableRow.appendChild(deleteButton);
     tableBody.appendChild(tableRow);
 }
